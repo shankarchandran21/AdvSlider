34a37d942489cf4e4af1f815133d4d11a972b458
Shankar <shankarchandran21@gmail.com>
2023-04-27 10:40:25 +0530

    Slider_setInterval

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { data } from "./data";
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1);
+    }
+
+    if (index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 2000);
+
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="container__heading">
+        <h2>Slider</h2>
+      </div>
+
+      <div className="container-flex">
+        {people.map(({ id, name, img, about }, personIndex) => {
+          let position = "nextSlide";
+          if (index === personIndex) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article key={id} className={position}>
+              <div className="user__container">
+                <div className="user__image-container">
+                  <img src={img} alt="user" />
+                  <div className="user__quotes__container">
+                    <FaQuoteRight className="user__quotes" />
+                  </div>
+                </div>
+                <h2>{name}</h2>
+                <p>{about}</p>
+              </div>
+            </article>
+          );
+        })}
+        <button
+          className="preButton"
+          type="button"
+          onClick={() => setIndex(index - 1)}
         >
-          Learn React
-        </a>
-      </header>
+          <FiChevronLeft />
+        </button>
+
+        <button
+          className="nextButton"
+          type="button"
+          onClick={() => setIndex(index + 1)}
+        >
+          <FiChevronRight />
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    name: "Mariya",
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sed ipsa ad, minima enim ullam laboriosam tempora praesentium labore consequuntur error, fugit tempore dolores dolorem doloribus alias, numquam a aspernatur.",
+  },
+  {
+    id: 2,
+    name: "Jhon",
+    img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sed ipsa ad, minima enim ullam laboriosam tempora praesentium labore consequuntur error, fugit tempore dolores dolorem doloribus alias, numquam a aspernatur.",
+  },
+  {
+    id: 3,
+    name: "Alex",
+    img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sed ipsa ad, minima enim ullam laboriosam tempora praesentium labore consequuntur error, fugit tempore dolores dolorem doloribus alias, numquam a aspernatur.",
+  },
+  {
+    id: 4,
+    name: "Luna",
+    img: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sed ipsa ad, minima enim ullam laboriosam tempora praesentium labore consequuntur error, fugit tempore dolores dolorem doloribus alias, numquam a aspernatur.",
+  },
+  {
+    id: 5,
+    name: "Stev",
+    img: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sed ipsa ad, minima enim ullam laboriosam tempora praesentium labore consequuntur error, fugit tempore dolores dolorem doloribus alias, numquam a aspernatur.",
+  },
+];
